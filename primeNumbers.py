96d11d424ef3842163dc17701a6ecdae9492bcb3
Galgallo Wako <gwako@SamaDc.corp>
2017-10-12 16:49:19 +0300

    capture exception

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,11 +1,16 @@
 def primeNumbers(n):
   isPrime = []
-  for num in range(2,n + 1):
-   
-   if num > 1:
-       for i in range(2,num):
-           if (num % i) == 0:
-               break
-       else:
-           isPrime.append(num)
-  return isPrime
+  if isinstance(n, (list, dict)):
+    raise TypeError("Number should be an integer") 
+  else:
+    for num in range(2,n + 1):
+     
+     if num > 1:
+         for i in range(2,num):
+             if (num % i) == 0:
+                 break
+         else:
+             isPrime.append(num)
+    return isPrime
+
+primeNumbers(9)
