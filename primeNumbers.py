2967a3faa283d1e5b200ddce24118965714c3cef
Galgallo Wako <galgallowako254@gmail.com>
2019-11-20 15:15:47 +0300

    Update primeNumbers.py

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,6 +1,8 @@
 def primeNumbers(n):
   # n integer 
   isPrime = []
+  if not isinstance(n, int):
+    raise TypeError('Input should be an integer')
   for num in range(2, n + 1):
     for i in range(2, num):
       if num % i == 0:
