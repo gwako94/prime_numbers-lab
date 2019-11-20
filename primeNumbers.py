e24d7047dc9fe7e6ffdd67cdee0c5c9efcaddf11
Galgallo Wako <galgallowako254@gmail.com>
2019-11-20 15:08:24 +0300

    Update primeNumbers.py

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,15 +1,11 @@
 def primeNumbers(n):
+  # n integer 
   isPrime = []
-  if isinstance(n, (list, dict, str)):
-    raise TypeError("Number should be an integer") 
-  else:
-    for num in range(2,n + 1):
-     
-     if num > 1:
-         for i in range(2,num):
-             if (num % i) == 0:
-                 break
-         else:
-             isPrime.append(num)
-    return isPrime
+  for num in range(2, n + 1):
+    for i in range(2, num):
+      if num % i == 0:
+        break
+    else:
+      isPrime.append(num) 
+  return isPrime
 
